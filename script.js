2fd908af7f51492293df77698d8c7549450bb908
Swathi Jettiboyina <swathi.pachipala@gmail.com>
2025-04-24 12:24:03 +0200

    hello world style commit

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
-const helloButton = document.getElementById("helloButton");
+document.addEventListener("DOMContentLoaded", function () {
+    const helloButton = document.getElementById("helloButton");
+    const heading = document.querySelector("h1");
 
+    helloButton.addEventListener("click", function () {
+        
+        heading.remove();
 
-helloButton.addEventListener("click", function () {
+        
+        helloButton.style.display = "none";
 
-    helloButton.style.display = "none";
+        
+        const message = document.createElement("div");
+        message.textContent = "Hello FRK24S again";
+        message.classList.add("new-message");
 
-
-    const message = document.createElement("div");
-    message.textContent = "Hello World again";
-
-    const container = document.querySelector(".container");
-    container.appendChild(message);
+        
+        const container = document.querySelector(".container");
+        container.appendChild(message);
+    });
 });
